b160535d5522298f2c6404ba921606b58a5edc96
OHMAE Ryosuke <ryo@mm2d.net>
2021-06-27 16:09:23 +0900

    🆙gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         targetSdkVersion(30)
         versionCode = versionMajor * 10000 + versionMinor * 100 + versionPatch
         versionName = "${versionMajor}.${versionMinor}.${versionPatch}"
-        base.archivesBaseName = "${applicationName}-${versionName}"
+        base.archivesName.set("${applicationName}-${versionName}")
         vectorDrawables.useSupportLibrary = true
     }
     compileOptions {
@@ -74,10 +74,10 @@ dependencies {
     kapt("com.github.bumptech.glide:compiler:4.12.0")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.7")
-    debugImplementation("com.facebook.flipper:flipper:0.92.0")
+    debugImplementation("com.facebook.flipper:flipper:0.95.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.1")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.92.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.92.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.95.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.95.0")
 }
 
 fun isStable(version: String): Boolean {
