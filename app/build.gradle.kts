116f76769e308c917840d6413502515368834b79
OHMAE Ryosuke <ryo@mm2d.net>
2021-11-20 11:17:36 +0900

    🆙jetpack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,21 +66,21 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("androidx.preference:preference-ktx:1.1.1")
     implementation("androidx.webkit:webkit:1.4.0")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
     implementation("com.squareup.okhttp3:okhttp:4.9.2")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.github.bumptech.glide:okhttp3-integration:4.12.0")
     kapt("com.github.bumptech.glide:compiler:4.12.0")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.7")
-    debugImplementation("com.facebook.flipper:flipper:0.119.0")
+    debugImplementation("com.facebook.flipper:flipper:0.120.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.3")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.119.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.119.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.120.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.120.0")
 }
 
 fun isStable(version: String): Boolean {
