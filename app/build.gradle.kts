6d0bd8c44c736069da3b481d0a0a8edfff4bb4bc
OHMAE Ryosuke <ryo@mm2d.net>
2022-01-26 21:59:00 +0900

    🆙AGP/flipper/jetpack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,21 +66,21 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.appcompat:appcompat:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.preference:preference-ktx:1.1.1")
     implementation("androidx.webkit:webkit:1.4.0")
-    implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.2")
+    implementation("com.google.android.material:material:1.5.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.github.bumptech.glide:okhttp3-integration:4.12.0")
     kapt("com.github.bumptech.glide:compiler:4.12.0")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.8.1")
-    debugImplementation("com.facebook.flipper:flipper:0.127.0")
+    debugImplementation("com.facebook.flipper:flipper:0.129.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.3")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.127.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.127.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.129.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.129.0")
 }
 
 fun isStable(version: String): Boolean {
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ buildscript {
         gradlePluginPortal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:7.1.0")
         classpath(kotlin("gradle-plugin", version = "1.6.10"))
-        classpath("com.github.ben-manes:gradle-versions-plugin:0.39.0")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.41.0")
     }
 }
 
