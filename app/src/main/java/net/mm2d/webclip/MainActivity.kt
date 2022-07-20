4ddf49d38d32a9a4de80239de7f9bafc7807bdb1
OHMAE Ryosuke <ryo@mm2d.net>
2022-07-20 22:26:24 +0900

    ♻OnBackPressedCallback

--- app/src/main/java/net/mm2d/webclip/MainActivity.kt
+++ app/src/main/java/net/mm2d/webclip/MainActivity.kt
@@ -16,6 +16,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.webkit.*
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.webkit.WebSettingsCompat
@@ -27,6 +28,11 @@ import net.mm2d.webclip.dialog.IconDialog
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val onBackPressedCallback: OnBackPressedCallback = object : OnBackPressedCallback(false) {
+        override fun handleOnBackPressed() {
+            binding.webView.goBack()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,6 +79,7 @@ class MainActivity : AppCompatActivity() {
             binding.editUrl.setText("")
             binding.drawerLayout.closeDrawer(GravityCompat.START)
         }
+        onBackPressedDispatcher.addCallback(onBackPressedCallback)
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -88,14 +95,6 @@ class MainActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(KEY_URL, binding.webView.url)
-    }
-
-    override fun onBackPressed() {
-        if (binding.webView.canGoBack()) {
-            binding.webView.goBack()
-            return
-        }
-        super.onBackPressed()
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -141,6 +140,10 @@ class MainActivity : AppCompatActivity() {
             }
         }
         binding.webView.webViewClient = object : WebViewClient() {
+            override fun doUpdateVisitedHistory(view: WebView, url: String, isReload: Boolean) {
+                onBackPressedCallback.isEnabled = view.canGoBack()
+            }
+
             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                 binding.progressBar.progress = 0
                 binding.progressBar.visibility = View.VISIBLE
