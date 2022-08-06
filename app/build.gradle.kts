eefc12b0a64cb54acfe71b338601174cb6020064
OHMAE Ryosuke <ryo@mm2d.net>
2022-08-06 16:16:00 +0900

    🆙AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,17 +75,18 @@ dependencies {
 
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.webkit:webkit:1.4.0")
     implementation("com.google.android.material:material:1.6.1")
-    implementation("com.google.dagger:hilt-android:2.42")
-    kapt("com.google.dagger:hilt-android-compiler:2.42")
+    implementation("com.google.dagger:hilt-android:2.43.2")
+    kapt("com.google.dagger:hilt-android-compiler:2.43.2")
 
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
     implementation("com.github.bumptech.glide:glide:4.13.2")
@@ -93,10 +94,10 @@ dependencies {
     kapt("com.github.bumptech.glide:compiler:4.13.2")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
-    debugImplementation("com.facebook.flipper:flipper:0.153.0")
+    debugImplementation("com.facebook.flipper:flipper:0.157.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.4")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.153.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.153.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.157.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.157.0")
 
     // for release
 }
