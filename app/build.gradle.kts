fc765eaa77f521326e88bd552bd01f31c3e6d6be
OHMAE Ryosuke <ryo@mm2d.net>
2022-09-17 00:28:44 +0900

    🆙AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.appcompat:appcompat:1.5.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
     implementation("androidx.preference:preference-ktx:1.2.0")
@@ -94,10 +94,10 @@ dependencies {
     kapt("com.github.bumptech.glide:compiler:4.13.2")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
-    debugImplementation("com.facebook.flipper:flipper:0.162.0")
+    debugImplementation("com.facebook.flipper:flipper:0.164.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.4")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.162.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.162.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.164.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.164.0")
 
     // for release
 }
