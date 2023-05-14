a960fa1f175be9a7467570449319df8eab450219
OHMAE Ryosuke <ryo@mm2d.net>
2023-05-14 11:29:43 +0900

    🆙AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,8 +78,8 @@ dependencies {
 
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -87,27 +87,27 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.webkit:webkit:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("com.google.dagger:hilt-android:2.45")
-    kapt("com.google.dagger:hilt-android-compiler:2.45")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.dagger:hilt-android:2.46.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.46.1")
 
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.github.bumptech.glide:glide:4.15.1")
     implementation("com.github.bumptech.glide:okhttp3-integration:4.15.1")
     kapt("com.github.bumptech.glide:compiler:4.15.1")
 
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.10")
-    debugImplementation("com.facebook.flipper:flipper:0.189.0")
+    debugImplementation("com.facebook.flipper:flipper:0.190.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.5")
-    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.189.0")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.189.0")
+    debugImplementation("com.facebook.flipper:flipper-network-plugin:0.190.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.190.0")
 
     // for release
 }
 
 fun isStable(version: String): Boolean {
     val hasStableKeyword =
-        listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+        listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     return hasStableKeyword || regex.matches(version)
 }
